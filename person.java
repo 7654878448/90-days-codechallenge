fef8c47459a92cee6af0041c40729f545ccfb6a9
Sumit Sehgal <126518503+7654878448@users.noreply.github.com>
2024-02-18 10:46:10 +0530

    Add files via upload

---
+++ person.java
@@ -0,0 +1,19 @@
+package com.ibm.aadhar;
+
+public class person {
+	public static void Speak()
+	{
+		System.out.println("hello i am sumit");
+	}
+	public static void m1()
+	{
+		System.out.println("i am from m1");
+	}
+	public static void m2()
+	{
+		System.out.println("i am from m2 method");
+	}
+	
+	
+
+}
