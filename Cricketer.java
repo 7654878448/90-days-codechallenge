c0b72269d9fe82e4aa87ec76d53c16156b03bf44
Sumit Sehgal <126518503+7654878448@users.noreply.github.com>
2024-04-25 14:27:29 +0530

    Add files via upload

--- Cricketer.java
+++ Cricketer.java
@@ -1,98 +1,59 @@
-package annotation_based_connection;
+package nativesql;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="cricketers1")
-
+@Table(name="nativecricketers")
 public class Cricketer {
-	@Id
-	 @Column(name= "jn")
-	private int jn;
-	 @Column(name= "playername")
-	
-	private String name;
-	 @Column(name= "adress")
-	private String adress;
-	 @Column(name= "mobile")
-	private long mobile;
-	 @Column(name= "adhar")
-	private long adhar;
-	 @Column(name= "iplteam")
-	private String iplteam;
-	 @Embedded
-	 private Account acc;
-	 @Embedded
-	 private Education edu;
-	public Cricketer(int jn, String name, String adress, long mobile, long adhar, String iplteam, Account acc,
-			Education edu) {
-		super();
-		this.jn = jn;
-		this.name = name;
-		this.adress = adress;
-		this.mobile = mobile;
-		this.adhar = adhar;
-		this.iplteam = iplteam;
-		this.acc = acc;
-		this.edu = edu;
-	}
-	public Cricketer() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public int getJn() {
-		return jn;
-	}
-	public void setJn(int jn) {
-		this.jn = jn;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAdress() {
-		return adress;
-	}
-	public void setAdress(String adress) {
-		this.adress = adress;
-	}
-	public long getMobile() {
-		return mobile;
-	}
-	public void setMobile(long mobile) {
-		this.mobile = mobile;
-	}
-	public long getAdhar() {
-		return adhar;
-	}
-	public void setAdhar(long adhar) {
-		this.adhar = adhar;
-	}
-	public String getIplteam() {
-		return iplteam;
-	}
-	public void setIplteam(String iplteam) {
-		this.iplteam = iplteam;
-	}
-	public Account getAcc() {
-		return acc;
-	}
-	public void setAcc(Account acc) {
-		this.acc = acc;
-	}
-	public Education getEdu() {
-		return edu;
-	}
-	public void setEdu(Education edu) {
-		this.edu = edu;
-	}
-	
-	
+    @Id
+    @Column(name="jn")
+    private int jn;
 
+    @Column(name="adress", length = 30)
+    private String adress;
+
+    @Column(name="salery") // Corrected column name to "salery"
+    private int salery;
+
+    public Cricketer() {
+        // Default constructor
+    }
+
+    public Cricketer(int jn, String adress, int salery) {
+        this.jn = jn;
+        this.adress = adress;
+        this.salery = salery;
+    }
+
+    public int getJn() {
+        return jn;
+    }
+
+    public void setJn(int jn) {
+        this.jn = jn;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getSalery() {
+        return salery;
+    }
+
+    public void setSalery(int salery) {
+        this.salery = salery;
+    }
+
+    @Override
+    public String toString() {
+        return "Cricketer [jn=" + jn + ", adress=" + adress + ", salery=" + salery + "]";
+    }
 }
--- launch.java
+++ launch.java
@@ -1,65 +1,114 @@
-package hbn_first_project;
+package nativesql;
 
+import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.NativeQuery;
 
 public class launch {
 	public static void main(String[]args)
 	{
-		Session session2=getsession();
+		Session ses=getsession();
+		System.out.println(ses);
 		Scanner sc=new Scanner(System.in);
-		student stu=session2.load(student.class,233);//it is quries of select8 from table name
-		//insert(session2,sc);
-		System.out.println("enter your secret code");
-		if(sc.nextInt()==1234)
+		
+		while(true)
 		{
-		System.out.println(stu.toString());
-		}
-		else
-		{
-			System.out.println("your are from some other group");
-		}
-	}
-		
-		public static void insert(Session sn,Scanner sc)
-		{
-		System.out.println("enter the roll number");
-		int roll=sc.nextInt();
-		System.out.println("enter the name");
-		String name=sc.next();
-		System.out.println("enter the marks");
-		double marks=sc.nextDouble();
-		System.out.println("enter the fav subject");
-		String favsubject=sc.next();
-		student s1=new student(roll,name,marks,favsubject);
-		Transaction t1=sn.beginTransaction();
-		int pk=(int) sn.save(s1);
-		System.out.println(pk);
-		t1.commit();
-		sc.close();
-		}
-		
-		
-		
-		
+			System.out.println("enter the choice");
+			int choice=sc.nextInt();
+			if(choice==5) {
+				System.out.println("you will enter the wrong choice");
+				break;
+			}
+			switch(choice)
+			{
+			case 1:
+				insert(ses,sc);
+			break;
+			case 2:
+			read(ses,sc);
+			break;
+			case 3:
+				//update();
+				break;
+			case 4:
+				//delete();
+				break;
+				default:
+					break;
 	
-	private static Session getsession()
+			}
+		}
+		
+	}
+	public static void read(Session scr,Scanner sc)
 	{
-	Configuration con=new Configuration();
-	con.configure("hbn.cfg.xml");
-	SessionFactory sun=con.buildSessionFactory();
-	Session ses=sun.openSession();
-	
-	return ses;
+		String sql="select * from nativecricketers";
+		NativeQuery nQuery=scr.createNativeQuery(sql);
+		List<Object[]> ls=nQuery.getResultList();
+		for(Object[] orr:ls)
+		{
+			for(Object obj:orr)
+			{
+				System.out.println(obj);
+			}
+			System.out.println();
+			
+		}
+		//System.out.println(ls);
+		
+		
 	}
+	public static void insert(Session scc,Scanner str)
+	{
+		Transaction tra=scc.beginTransaction();
+		System.out.println("enter the player jn number");
+		int jn=str.nextInt();
+		System.out.println("enter the name");
+		String name=str.next();
+		System.out.println("enter the adress");
+		String adress=str.next();
+		System.out.println("enter the salery");
+		int salery=str.nextInt();
+		
+		String sql="insert into nativecricketers(jn,name,adress,salery)values(:jn,:name,:adress,:salery)";
+		NativeQuery<?> nq=scc.createNativeQuery(sql);
+		nq.setParameter("jn", jn);
+		nq.setParameter("name",name);
+		nq.setParameter("adress",adress);
+		nq.setParameter("salery", salery);
+		
+		int row=nq.executeUpdate();
+		System.out.println(row);
+		tra.commit();
+		str.close();
+		
+		
+		
+	}
+	
+	public static Session getsession()
+	{
+		Configuration con=new Configuration();
+		Properties pro=new Properties();
+		pro.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
+		pro.put(Environment.URL,"jdbc:mysql://localhost:3306/cricketer");
+		pro.put(Environment.USER,"root");
+		pro.put(Environment.PASS,"12345");
+		pro.put(Environment.SHOW_SQL,true);
+		pro.put(Environment.HBM2DDL_AUTO,"update");
+		con.setProperties(pro);
+		con.addAnnotatedClass(Cricketer.class);
+		Session str=con.buildSessionFactory().openSession();
+		return str;	
+	}
+	
+	
+	
 
 }
-/*
-Caching: Think of caching as a temporary storage area. The Session has a cache where it keeps recently accessed data. This helps speed up your application because if you need the same data again, it can be fetched quickly from this cache instead of going all the way to the database.
-Lazy Loading: Lazy loading is like being efficient with resources. When you're working with a lot of data, you don't want to load everything at once. The Session helps by loading only what you need when you need it. For example, if you have a list of items and each item has details, lazy loading ensures that the details are fetched only when you ask for them, not all at once.
-Identity Map: Imagine you have a list of students, and each student has a unique roll number. The Session keeps track of which roll number corresponds to which student object. This way, if you ask for the student with roll number 123 twice, the Session gives you the same object both times, ensuring consistency.
-*/
