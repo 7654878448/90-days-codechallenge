b526010f25ca4f193b6719fd7eaef7a6c78d34c6
Sumit Sehgal <126518503+7654878448@users.noreply.github.com>
2024-06-24 14:53:35 +0530

    Add files via upload

---
+++ myconfiguration.java
@@ -0,0 +1,13 @@
+package com.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.spring.google;
+
+@Configuration
+@ComponentScan(basePackages = ("com.spring"))
+
+public class myconfiguration {
+
+}
